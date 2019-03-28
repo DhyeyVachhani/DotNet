b0794dc36cde5caf36fb577131303d0f330ea916
DhyeyVachhani <dhyey890@gmail.com>
2019-03-29 00:34:44 +0530

    all assignments

---
+++ assignment-2/country.cs
@@ -0,0 +1,25 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace country
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string country;
+            Console.WriteLine("enter your name:");
+            name=Console.ReadLine();
+            Console.WriteLine("enter your country:");
+            country = Console.ReadLine();
+            Console.WriteLine("hello {0} from country {1}",name,country);
+            Console.ReadKey();
+        }
+        
+    }
+}
